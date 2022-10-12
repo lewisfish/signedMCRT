cbde44ad7c62748b3c2707b148cc644eed52beed
lewis <lewistmcmillan@gmail.com>
2022-10-12 11:32:55 +0100

    Added 1 test to detector and matrix suites.

--- test/detector/test_detector.f90
+++ test/detector/test_detector.f90
@@ -12,14 +12,48 @@ module testsDetectorMod
 
         type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
-        ! testsuite = [ &
-                ! new_unittest("Vector_add", vector_add) &
+        testsuite = [ &
+                new_unittest("Check_hit_circle", hit_circle) &
                 ! ! new_unittest("Vector_subtract", vector_sub), &
                 ! ! new_unittest("Vector_multiply", vector_mult), &
                 ! ! new_unittest("Vector_dot", vector_dot) &
-                ! ]
+                ]
 
     end subroutine collect_suite1
+
+    subroutine hit_circle(error)
+
+        use vector_class, only : vector
+
+        type(error_type), allocatable, intent(out) :: error
+
+        type(hit_t) :: hitpoint
+        type(circle_dect) :: a
+        type(vector) :: pos, dir
+        integer :: layer, nbins
+        real(kind=wp) :: radius, maxval, val
+        logical :: flag
+
+        pos = vector(0._wp, 0._wp, 0._wp)
+        dir = vector(1._wp, 0._wp, 0._wp)
+        layer = 1
+        radius = 0.5
+        nbins = 100
+        maxval = 100._wp
+        a = circle_dect(pos, dir, layer, radius, nbins, maxval, .false.)
+
+        pos = vector(0._wp, 0._wp, 0._wp)
+        dir = vector(1._wp, 0._wp, 0._wp)
+        val = 1._wp
+        layer = 1
+        hitpoint = hit_t(pos, dir, val, layer)
+
+        flag = a%check_hit(hitpoint)
+
+        call check(error, flag, .true.)
+        if(allocated(error))return
+
+    end subroutine hit_circle
 
 end module testsDetectorMod
 program test_detector
@@ -38,16 +72,16 @@ program test_detector
 
     stat = 0
 
-    ! testsuites = [new_testsuite("Suite: Vector .op. vector", collect_suite1), &
+    testsuites = [new_testsuite("Suite: Check Hits", collect_suite1) &
     !               new_testsuite("Suite: Vector .op. scalar", collect_suite2), &
     !               new_testsuite("Suite: Vector functions", collect_suite3), &
     !               new_testsuite("Suite: Vector .op. matrix", collect_suite4) &
-    !               ]
+                  ]
 
-    ! do i = 1, size(testsuites)
-    !     write(error_unit, fmt) "Testing:", testsuites(i)%name
-    !     call run_testsuite(testsuites(i)%collect, error_unit, stat)
-    ! end do
+    do i = 1, size(testsuites)
+        write(error_unit, fmt) "Testing:", testsuites(i)%name
+        call run_testsuite(testsuites(i)%collect, error_unit, stat)
+    end do
 
     if(stat > 0)then
         write(error_unit, '(i0, 1x, a)')stat, "test(s) failed"
--- test/matrix/test_matrix.f90
+++ test/matrix/test_matrix.f90
@@ -12,15 +12,38 @@ module testsMatrixMod
 
         type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
-        ! testsuite = [ &
-                ! new_unittest("Vector_add", vector_add) &
+        testsuite = [ &
+                new_unittest("Matrix_add", Matrix_add) &
                 ! ! new_unittest("Vector_subtract", vector_sub), &
                 ! ! new_unittest("Vector_multiply", vector_mult), &
                 ! ! new_unittest("Vector_dot", vector_dot) &
-                ! ]
+                ]
 
     end subroutine collect_suite1
 
+    subroutine matrix_add(error)
+
+        type(error_type), allocatable, intent(out) :: error
+
+        real(kind=wp) :: a(4,4), b, c(4,4), val
+        integer :: i, j
+
+        a(:, 1) = [1._wp, 2._wp, 3._wp, 4._wp] 
+        a(:, 2) = [1._wp, 2._wp, 3._wp, 4._wp] 
+        a(:, 3) = [1._wp, 2._wp, 3._wp, 4._wp] 
+        a(:, 4) = [1._wp, 2._wp, 3._wp, 4._wp] 
+        b = 5._wp
+
+        c = a + b
+        val = 6._wp
+        do i = 1, 4
+            do j = 1, 4
+                call check(error, c(i,j), val)
+                if(allocated(error))return
+            end do
+            val = val + 1._wp
+        end do
+    end subroutine matrix_add
 end module testsMatrixMod
 program test_matrix
 
@@ -38,16 +61,16 @@ program test_matrix
 
     stat = 0
 
-    ! testsuites = [new_testsuite("Suite: Vector .op. vector", collect_suite1), &
+    testsuites = [new_testsuite("Suite: Matrix .op. scalar", collect_suite1) &
     !               new_testsuite("Suite: Vector .op. scalar", collect_suite2), &
     !               new_testsuite("Suite: Vector functions", collect_suite3), &
     !               new_testsuite("Suite: Vector .op. matrix", collect_suite4) &
-    !               ]
+                  ]
 
-    ! do i = 1, size(testsuites)
-    !     write(error_unit, fmt) "Testing:", testsuites(i)%name
-    !     call run_testsuite(testsuites(i)%collect, error_unit, stat)
-    ! end do
+    do i = 1, size(testsuites)
+        write(error_unit, fmt) "Testing:", testsuites(i)%name
+        call run_testsuite(testsuites(i)%collect, error_unit, stat)
+    end do
 
     if(stat > 0)then
         write(error_unit, '(i0, 1x, a)')stat, "test(s) failed"
